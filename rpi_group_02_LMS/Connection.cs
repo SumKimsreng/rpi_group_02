85cc0a669814b0944fa9b546a823545bcbeb60f5
ASUS <ASUS@DESKTOP-O7CE13G>
2024-08-02 00:02:31 +0700

    connection class was created

---
+++ rpi_group_02_LMS/Connection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpi_group_02_LMS
+{
+    internal class Connection
+    {
+    }
+}
